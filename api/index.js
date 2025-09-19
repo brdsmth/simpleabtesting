66a705944918b8416c8f001274248f346cce3ba8
brdsmth <brsmith110@gmail.com>
2025-09-19 15:02:36 -0400

    fix(api): basic catch all route

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ app.get('/sdk.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'sdk', 'dist', 'simple-ab-testing.umd.js'));
 });
 
+app.get('/', (req, res) => {
+  res.send("Welcome to the Simple A/B Testing API");
+});
+
 app.listen(port, () => {
   console.log(`API server listening at http://localhost:${port}`);
 });
